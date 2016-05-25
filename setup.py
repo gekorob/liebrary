66894c21535a6e3f65856bda43b81bc2476d4d17
Roberto Ciatti <roberto.ciatti@gmail.com>
2016-05-25 11:11:06 +0200

    initial commit with initial structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='liebrary',
+    version='0.1',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    packages=find_packages(),
+    description="Liebrary application",
+    long_description="",
+    classifiers=(
+    ),
+    install_requires=[],
+    scripts=[]
+)
